3f9540d2c599a367dad02b5f910d74a00b0348af
Nazar Serhiienko <nazarik20052203@gmail.com>
2025-12-17 13:54:28 +0200

    Lab2 everything works

---
+++ src/main/java/com/example/beans/Department.java
@@ -0,0 +1,34 @@
+package com.example.beans;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Department {
+    @Value("${department.name}")
+    private String name;
+    @Value("${department.code}")
+    private String code;
+
+    private final Dean dean;
+
+    public Department(Dean dean) {
+        this.dean = dean;
+    }
+    @PostConstruct
+    public void init() {
+        System.out.println("Department init: " + name + " " + code);
+    }
+
+    @PreDestroy
+    public void destroy() {
+        System.out.println("Department destroy: " + name);
+    }
+
+    @Override
+    public String toString() {
+        return "\nDepartment{name: '" + name + "'" + ", code: " + code + ", dean=" + dean + "}";
+    }
+}
